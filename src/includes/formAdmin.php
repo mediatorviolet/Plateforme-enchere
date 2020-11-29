e10b9785e886d3ff57b117977c8536672f642bd8
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-29 12:55:03 +0400

    message erreur

--- src/includes/formAdmin.php
+++ src/includes/formAdmin.php
@@ -9,11 +9,11 @@ if (isset($_POST["submit"])) {
 
 <div class="container-fluid p-lg-5 p-md-3">
   <h2 class="display-4 text-center p-lg-5 p-md-3 py-3">Mettre un produit en vente</h2>
-  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
     <div class="form-group">
       <label for="nomProduit">Nom du produit</label>
-      <input type="text" class="form-control" id="nomProduit" name="nomProduit" required>
-      <span class="error"><?php echo $nomProduitErr; ?></span>
+      <input type="text" class="form-control" id="nomProduit" name="nomProduit">
+      <span class="alert-danger"><?php echo $nomProduitErr; ?></span>
     </div>
     <div class="form-group">
       <label for="inputUploadImg">Ajouter une image</label>
@@ -22,23 +22,27 @@ if (isset($_POST["submit"])) {
     <div class="form-group">
       <label for="inputPrixLancement">Prix de lancement (€)</label>
       <input type="number" class="form-control" id="inputPrixLancement" name="inputPrixLancement">
-      <span class="error"><?php echo $inputPrixLancementErr; ?></span>
+      <span class="alert-danger"><?php echo $inputPrixLancementErr; ?></span>
     </div>
     <div class="form-group">
       <label for="inputDuree">Durée de l'enchère (h)</label>
       <input type="number" class="form-control" id="inputDuree" name="inputDuree">
+      <span class="alert-danger"><?php echo $inputDureeErr; ?></span>
     </div>
     <div class="form-group">
       <label for="inputPrixClic">Prix du clic (cts)</label>
       <input type="number" class="form-control" id="inputPrixClic" name="inputPrixClic">
+      <span class="alert-danger"><?php echo $inputPrixClicErr; ?></span>
     </div>
     <div class="form-group">
       <label for="inputAugmentationPrix">Augmentation du prix par clic (cts)</label>
       <input type="number" class="form-control" id="inputAugmentationPrix" name="inputAugmentationPrix">
+      <span class="alert-danger"><?php echo $inputAugmentationPrixErr; ?></span>
     </div>
     <div class="form-group">
       <label for="inputAugmentationDuree">Augmentation de la durée par clic (s)</label>
       <input type="number" class="form-control" id="inputAugmentationDuree" name="inputAugmentationDuree">
+      <span class="alert-danger"><?php echo $inputAugmentationDureeErr; ?></span>
     </div>
     <button type="submit" name="submit" class="btn btn-dark">Ajouter le produit</button>
   </form>
