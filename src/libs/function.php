9590aa9cfc65949fd9b315872377f2b747efb2ab
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-30 17:10:15 +0400

    ajout enchère avec json fonctionne

--- src/libs/function.php
+++ src/libs/function.php
@@ -1,7 +1,8 @@
 <?php
-require_once 'data.php';
-$nomProduit = $inputPrixLancement = $inputDuree = $inputPrixClic = $inputAugmentationPrix = $inputAugmentationDuree = "";
+require_once 'upload.php';
+//require_once 'data.json';
 $nomProduitErr = $inputPrixLancementErr = $inputDureeErr = $inputPrixClicErr = $inputAugmentationPrixErr = $inputAugmentationDureeErr = "";
+/*$nomProduit = $inputPrixLancement = $inputDuree = $inputPrixClic = $inputAugmentationPrix = $inputAugmentationDuree = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(empty($_POST["nomProduit"])) {
@@ -34,15 +35,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $inputAugmentationDuree = validationForm($_POST["inputAugmentationDuree"]);
     }
-}
+}*/
 
+
+$class_alert = "";
+$msg_alert = "";
 function validationForm() {
+    global $class_alert;
+    global $msg_alert;
+    $count = 0;
     $required_input = ["nomProduit", "inputPrixLancement", "inputDuree", "inputPrixClic", "inputAugmentationPrix", "inputAugmentationDuree"];
     foreach ($required_input as $input) {
         if (empty($_POST["$input"])) {
-            $form_validation = false;
+            //$form_validation = false;
+            $count++;
         } else {
-            $form_validation = true;
+            //$form_validation = true;
 
             $_POST["$input"] = trim($_POST["$input"]);
             $_POST["$input"] = stripslashes($_POST["$input"]);
@@ -50,24 +58,44 @@ function validationForm() {
         }
         //echo $_POST["$input"] . "<br>";
     }
-    if ($form_validation == false) {
-        echo "<div class=\"col-6 d-flex justify-content-center\">    
-        <div class=\"alert alert-danger\">Veuillez remplir tous les champs demandés.</div></div>";
+    if ($count > 0) {
+        /*echo "<div class=\"col-6 d-flex justify-content-center\">    
+        <div class=\"alert alert-danger\">Veuillez remplir tous les champs demandés.</div></div>";*/
+        $class_alert = "danger";
+        $msg_alert = "Veuillez remplir tous les champs demandés.";
     } else {
-        echo "<div class=\"col-6 d-flex justify-content-center\">    
-        <div class=\"alert alert-success\">Article ajouté avec succès.</div></div>";
-    
-        // Création d'un id unique pour chaque article
-        $id_enchere = "article_" . md5(uniqid(rand(), true));
-        $_POST['id'] = $id_enchere;
-        
-        // Création d'un nouvel objet dans la classe Articles
-        $data_file = 'src/libs/data.php';
-        $text = "<?php " . $_POST["id"] . " = new Articles(\"" . $_POST["nomProduit"] . "\", \"src/resources/img/uploads/" 
-        . htmlspecialchars(basename($_FILES["inputUploadImg"]["name"])) . "\", " . $_POST["inputPrixLancement"] . ", " 
-        . $_POST["inputDuree"] . ", " . $_POST["inputPrixClic"] . ", " . $_POST["inputAugmentationPrix"] . ", " 
-        . $_POST["inputAugmentationDuree"] . ")" . "; ?>";
-        file_put_contents($data_file, $text, FILE_APPEND);
+        /*echo "<div class=\"col-6 d-flex justify-content-center\">    
+        <div class=\"alert alert-success\">Article ajouté avec succès.</div></div>";*/
+        $class_alert = "success";
+        $msg_alert = "Article ajouté avec succès.";
+        ajout_produit();
     }
 }
+
+
+function ajout_produit() {
+    // Création d'un id unique pour chaque article
+    $id_enchere = "article_" . md5(uniqid(rand(), true));
+    $_POST["id"] = $id_enchere;
+    
+    // Ajout du de l'article dans le tableau $json_array
+    $postArray = array(
+        $id_enchere => array(
+            "titre" => $_POST["nomProduit"],
+            "image" => "src/resources/img/uploads/" . basename($_FILES["inputUploadImg"]["name"]),
+            "prixLancement" => $_POST["inputPrixLancement"],
+            "duree" => $_POST["inputDuree"],
+            "prixClic" => $_POST["inputPrixClic"],
+            "augmentationPrix" => $_POST["inputAugmentationPrix"],
+            "augmentationDuree" => $_POST["inputAugmentationDuree"]
+        )
+    );
+
+    $data_file = 'src/libs/data.json';
+    $json_array = json_decode(file_get_contents($data_file), true);
+    array_push($json_array, $postArray);
+    file_put_contents($data_file, json_encode($json_array));
+    
+}
+
 ?>
