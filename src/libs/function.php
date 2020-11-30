e643532c266b3051de0c55e4dd496ff0e3ce2da6
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-11-30 17:14:35 +0400

    clean function.php

--- src/libs/function.php
+++ src/libs/function.php
@@ -1,41 +1,28 @@
 <?php
 require_once 'upload.php';
-//require_once 'data.json';
 $nomProduitErr = $inputPrixLancementErr = $inputDureeErr = $inputPrixClicErr = $inputAugmentationPrixErr = $inputAugmentationDureeErr = "";
-/*$nomProduit = $inputPrixLancement = $inputDuree = $inputPrixClic = $inputAugmentationPrix = $inputAugmentationDuree = "";
+$nomProduit = $inputPrixLancement = $inputDuree = $inputPrixClic = $inputAugmentationPrix = $inputAugmentationDuree = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(empty($_POST["nomProduit"])) {
         $nomProduitErr = "Veuillez entrer un nom pour votre produit.";
-    } else {
-        $nomProduit = validationForm($_POST["nomProduit"]);
     }
     if(empty($_POST["inputPrixLancement"])) {
         $inputPrixLancementErr = "Veuillez entrer un prix de lancement.";
-    } else {
-        $inputPrixLancement = validationForm($_POST["inputPrixLancement"]);
     }
     if(empty($_POST["inputDuree"])) {
         $inputDureeErr = "Veuillez entrer une durée.";
-    } else {
-        $inputDuree = validationForm($_POST["inputDuree"]);
     }
     if(empty($_POST["inputPrixClic"])) {
         $inputPrixClicErr = "Veuillez entrer le prix du clic.";
-    } else {
-        $inputPrixClic = validationForm($_POST["inputPrixClic"]);
     }
     if(empty($_POST["inputAugmentationPrix"])) {
         $inputAugmentationPrixErr = "Veuillez entrer une valeur pour l'augmentation du prix par clic.";
-    } else {
-        $inputAugmentationPrix = validationForm($_POST["inputAugmentationPrix"]);
     }
     if(empty($_POST["inputAugmentationDuree"])) {
         $inputAugmentationDureeErr = "Veuillez entrer une valeur pour l'augmentation de la durée de l'enchère par clic.";
-    } else {
-        $inputAugmentationDuree = validationForm($_POST["inputAugmentationDuree"]);
     }
-}*/
+}
 
 
 $class_alert = "";
@@ -47,25 +34,17 @@ function validationForm() {
     $required_input = ["nomProduit", "inputPrixLancement", "inputDuree", "inputPrixClic", "inputAugmentationPrix", "inputAugmentationDuree"];
     foreach ($required_input as $input) {
         if (empty($_POST["$input"])) {
-            //$form_validation = false;
             $count++;
         } else {
-            //$form_validation = true;
-
             $_POST["$input"] = trim($_POST["$input"]);
             $_POST["$input"] = stripslashes($_POST["$input"]);
             $_POST["$input"] = htmlspecialchars($_POST["$input"]);
         }
-        //echo $_POST["$input"] . "<br>";
     }
     if ($count > 0) {
-        /*echo "<div class=\"col-6 d-flex justify-content-center\">    
-        <div class=\"alert alert-danger\">Veuillez remplir tous les champs demandés.</div></div>";*/
         $class_alert = "danger";
         $msg_alert = "Veuillez remplir tous les champs demandés.";
     } else {
-        /*echo "<div class=\"col-6 d-flex justify-content-center\">    
-        <div class=\"alert alert-success\">Article ajouté avec succès.</div></div>";*/
         $class_alert = "success";
         $msg_alert = "Article ajouté avec succès.";
         ajout_produit();
