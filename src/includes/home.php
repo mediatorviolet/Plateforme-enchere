1174fb34d6185480fbbe9f8f96e62d0940d25d4b
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-12-01 15:09:36 +0400

    fix upload img

--- src/includes/home.php
+++ src/includes/home.php
@@ -1,14 +1,14 @@
+<?php
+  require_once "src/libs/function.php";
+  $data_file = 'src/libs/data.json';
+  $json_array = json_decode(file_get_contents($data_file), true);
+  //print_r($json_array);
+?>
 <div class="container-fluid p-lg-5 p-md-3">
 
   <h2 class="display-4 text-center p-lg-5 p-md-3 py-3">Produits en vente</h2>
   
   <div class="row row-cols-1 row-cols-md-3">
-    <?php
-      require_once "src/libs/function.php";
-      $data_file = 'src/libs/data.json';
-      $json_array = json_decode(file_get_contents($data_file), true);
-      //print_r($json_array);
-    ?>
     <?php foreach(array_reverse($json_array) as $key => $value) :?>
     <?php foreach($value as $val => $item) :?>
     <div class="col mb-4">
@@ -19,47 +19,33 @@
       <?= $item["duree"] ?></div>
         <div class="card-body">
           <h5 class="card-title font-weight-bold text-capitalize"><?= $item["titre"] ?></h5>
-          <p class="card-text mb-0">Prix de lancement : <?= $item["prixLancement"] ?></p>
-          <p class="card-text mb-0">Prix du clic : <?= $item["prixClic"] ?></p>
-          <p class="card-text mb-0">Augmentation du prix : <?= $item["augmentationPrix"] ?></p>
-          <p class="card-text">Augmentation de la durée : <?= $item["augmentationDuree"] ?></p>
+          <p class="card-text mb-0">Prix de lancement : <?= $item["prixLancement"] ?> €</p>
+          <p class="card-text mb-0">Prix du clic : <?= $item["prixClic"] ?> cts / clic</p>
+          <p class="card-text mb-0">Augmentation du prix : <?= $item["augmentationPrix"] ?> cts /clic</p>
+          <p class="card-text">Augmentation de la durée : <?= $item["augmentationDuree"] ?> s / clic</p>
+          <input type="hidden" name="hint" value="">
           <button type="button" class="btn btn-dark">Enchérir</button>
         </div>
       </div>
     </div>
+
+    <script>
+    //Gestion du timer by Vincent
+      var timer = setInterval(function countDown() {
+          var tempAct = new Date(); //On recupere la date UNIX
+          var heure = Math.floor(tempAct.getTime() / 1000); //On transforme la date en secondes depuis la date fixe UNIX
+          var timeRemaining = <?php echo $items['date_fin']?> - heure; //On compare les secondes depuis date fixe UNIX PHP à JS
+          var hoursRemaining = parseInt(timeRemaining / 3600); // conversion en heures
+          var minutesRemaining = parseInt((timeRemaining % 3600) / 60); // conversion en minutes
+          var secondsRemaining = parseInt((timeRemaining % 3600) % 60); // conversion en secondes
+          //On attribue l'id de l'enchere dans la zone où il y a le timer et on dit que l'on souhaite afficher le timer
+          document.getElementById('<?= $items['duree'] ?>').innerHTML = hoursRemaining + ' h : ' + minutesRemaining + ' m : ' + secondsRemaining + ' s ';
+          if (timeRemaining <= 0) {//Sinon on met expire et on desactive le bouton encherir
+              document.getElementById('<?= $items['duree'] ?>').innerHTML = "EXPIRE";
+          }
+      }, 1000); // répéte la fonction toutes les 1 seconde
+    </script>
     <?php endforeach; ?>
     <?php endforeach; ?>
-    <!-- <div class="col mb-4">
-      <div class="card h-100">
-        <img src="src/resources/img/aude-andre-saturnio-QXAUlcgWYj8-unsplash.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a short card.</p>
-          <button type="button" class="btn btn-dark">Enchérir</button>
-        </div>
-      </div>
-    </div>
-    <div class="col mb-4">
-      <div class="card h-100">
-        <img src="src/resources/img/kyrylo-kholopkin-JB91fiIa9RE-unsplash.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-            content.</p>
-          <button type="button" class="btn btn-dark">Enchérir</button>
-        </div>
-      </div>
-    </div>
-    <div class="col mb-4">
-      <div class="card h-100">
-        <img src="src/resources/img/wolfgang-hasselmann-g4pHm-NOliM-unsplash.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-            content. This content is a little bit longer.</p>
-          <button type="button" class="btn btn-dark">Enchérir</button>
-        </div>
-      </div>
-    </div> -->
   </div>
 </div>
--- src/libs/upload.php
+++ src/libs/upload.php
@@ -1,6 +1,6 @@
 <?php
 
-function img_upload() {
+/*function img_upload() {
     $target_dir = "src/resources/img/uploads/";
     $target_file = $target_dir . basename($_FILES["inputUploadImg"]["name"]);
     $uploadOk = 1;
@@ -49,5 +49,60 @@ function img_upload() {
             echo "Sorry, there was an error uploading your file.";
         }
     }
+}*/
+
+function img_upload() {
+    $fileName = $_FILES["inputUploadImg"]["name"];
+    if ($fileName !== "") { // On vérifie que la variable img n'est pas vide
+        $validExt = array(".jpg", ".jpeg", ".png", ".gif");
+        /*if ($_FILES["inputUploadImg"]["error"] > 0) {
+            echo "Une erreur est survenue lors du transfert de l'image.";
+            echo $_FILES["inputUploadImg"]["error"];
+            die;
+        }*/
+        $fileError = $_FILES["inputUploadImg"]["error"];
+        switch($fileError) {
+            case UPLOAD_ERR_INI_SIZE:
+                echo "Exceeds max size in php.ini";
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                echo "Exceeds max size in html form";
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                echo "No file was uploaded";
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+                echo "No /tmp dir to write to";
+                break;
+            case UPLOAD_ERR_CANT_WRITE:
+                echo "Error writing to disk";
+                break;
+            default:
+                echo "No error was faced! Phew!";
+                break;
+        }
+        if ($_FILES["inputUploadImg"]["size"] > 5000000000000000) {
+            echo "Le ficher est trop lourd.";
+            die;
+        }
+        $fileExt = strtolower(substr(strrchr($fileName, '.'), 1));
+        if (!in_array("." . $fileExt, $validExt)) {
+            echo "Une image valide est une image .jpg, .jpeg. png ou .gif.";
+            die;
+        }
+        $tmp_name = $_FILES["inputUploadImg"]["tmp_name"];
+        $idName = md5(uniqid(rand(), true));
+        $name = basename($_FILES["inputUploadImg"]["name"]);
+        $target_dir = "src/resources/img/uploads/" . $name;
+        $_POST['inputUploadImg'] = $name;
+        move_uploaded_file($tmp_name, "$target_dir");
+        /*$target_file = $target_dir . basename($_FILES["inputUploadImg"]["name"]);
+        //$_POST["inputUploadImg"] = $tmp_name . "." . $fileExt;
+        $_POST["inputUploadImg"] = $fileName;
+        $result = move_uploaded_file($fileName, $target_file);*/
+    } else {
+        $_POST["inputUploadImg"] = "src/resources/img/453x302.png";
+    }
 }
+
 ?>
