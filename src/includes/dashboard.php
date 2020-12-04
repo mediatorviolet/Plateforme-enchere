b0e76ca25b9bb1a131adcc9e878abc8dfc69f9b4
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-12-04 11:03:04 +0400

    timer dans le dashboard

--- src/includes/dashboard.php
+++ src/includes/dashboard.php
@@ -1,38 +1,66 @@
 <?php
-    $data_file = "src/libs/data.json";
-    $json_array = json_decode(file_get_contents($data_file), true);
+$data_file = "src/libs/data.json";
+$json_array = json_decode(file_get_contents($data_file), true);
 ?>
 
-<div class="container-fluid p-lg-5 p-md-3">
-    <h2 class="display-4 text-center p-lg-5 p-md-3 py-3">Tableau de bord</h2>
-    <table class=" table table-responsive table-hover table-dark">
-  <thead>
-    <tr>
-      <th class="align-middle text-center" scope="col">#</th>
-      <th class="align-middle text-center" scope="col">Titre</th>
-      <th class="align-middle text-center" scope="col">Image</th>
-      <th class="align-middle text-center" scope="col">Prix</th>
-      <th class="align-middle text-center" scope="col">Temps restant</th>
-      <th class="align-middle text-center" scope="col">Prix du clic</th>
-      <th class="align-middle text-center" scope="col">Augmentation du prix</th>
-      <th class="align-middle text-center" scope="col">Augmentation de la durée</th>
-      <th class="align-middle text-center" scope="col">Etat</th>
-    </tr>
-  </thead>
-  <tbody>
-      <?php foreach($json_array as $key => $value) :?>
-    <tr>
-      <th class="align-middle text-center" scope="row"><?= $key ?></th>
-      <td class="align-middle text-center"><?= $value["titre"] ?></td>
-      <td><img src="<?= $value["image"] ?>" alt="" class="img-thumbnail" style="max-width: 150px"></td>
-      <td class="align-middle text-center"><?= $value["prixLancement"] ?></td>
-      <td class="align-middle text-center">Temps restant ??</td>
-      <td class="align-middle text-center"><?= $value["prixClic"] ?></td>
-      <td class="align-middle text-center"><?= $value["augmentationPrix"] ?></td>
-      <td class="align-middle text-center"><?= $value["augmentationDuree"] ?></td>
-      <td class="align-middle text-center"><?= $value["etat"] ?></td>
-    </tr>
-    <?php endforeach ?>
-  </tbody>
-</table>
+<div class="container-fluid ">
+  <h2 class="display-4 text-center p-lg-5 p-md-3 py-3">Tableau de bord</h2>
+  <table class=" table table-responsive table-hover table-dark">
+    <thead>
+      <tr>
+        <th class="align-middle text-center" scope="col">#</th>
+        <th class="align-middle text-center" scope="col">Titre</th>
+        <th class="align-middle text-center" scope="col">Image</th>
+        <th class="align-middle text-center" scope="col">Prix</th>
+        <th class="align-middle text-center" scope="col">Temps restant</th>
+        <th class="align-middle text-center" scope="col">Prix du clic</th>
+        <th class="align-middle text-center" scope="col">Augmentation du prix</th>
+        <th class="align-middle text-center" scope="col">Augmentation de la durée</th>
+        <th class="align-middle text-center" scope="col">Etat</th>
+        <th class="align-middle text-center" scope="col">Activer / Désactiver</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($json_array as $key => $value) : ?>
+        <tr>
+          <th class="align-middle text-center" scope="row"><?= $key ?></th>
+          <td class="align-middle text-center"><?= $value["titre"] ?></td>
+          <td><img src="<?= $value["image"] ?>" alt="" class="img-thumbnail" style="max-width: 150px"></td>
+          <td class="align-middle text-center"><?= $value["prixLancement"] ?></td>
+          <td id="duree_<?= $key ?>" class="align-middle text-center"><?php include "src/libs/timer.php" ?></td>
+          <td class="align-middle text-center"><?= $value["prixClic"] ?></td>
+          <td class="align-middle text-center"><?= $value["augmentationPrix"] ?></td>
+          <td class="align-middle text-center"><?= $value["augmentationDuree"] ?></td>
+          <td class="align-middle text-center text-uppercase font-weight-bold <?= $value["etat"] == "actif" ? "text-success" : "text-danger" ?>"><?= $value["etat"] ?></td>
+          <td class="align-middle text-center">
+            
+          </td>
+        </tr>
+      <?php endforeach ?>
+    </tbody>
+  </table>
 </div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<?php
+//Cette partie sert à rafraichir les timers de chaque enchère
+$json_array = json_decode(file_get_contents('src/libs/data.json'), true);
+for ($x = 0; $x < count($json_array); $x++) {     //On récupère la longueur du tableau contenant des produits, on rafraichit le temps de chaque enchère
+?>
+
+
+  <script>
+    $(document).ready(function() {
+
+      function myFunction() {
+        var myVar = setInterval(function() {
+          $('<?php echo '#duree_' . $x ?>').load('admin.php?page=dashboard <?php echo '#duree_' . $x ?>');
+        }, 1000);
+      };
+
+      myFunction();
+    })
+  </script>
+
+<?php } ?>
